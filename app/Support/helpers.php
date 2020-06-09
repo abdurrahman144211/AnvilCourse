47d59b372b9aae39741c31a812af6b78f487a1b3
Abdurrahman Faid <abdurrahman144211@gmail.com>
2020-06-09 20:39:31 +0200

    Install laravel [debugger, telescope, horizon]

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -1,16 +1,5 @@
 <?php
 
-if(! function_exists('appPresent')) {
-    function appPresent($key = null)
-    {
-        $settings =  \Cache::rememberForever('app.present', function () {
-            return resolve(\App\Repositories\Contracts\AppSettingsRepositoryInterface::class)->first();
-        });
-
-        return $key ? $settings[$key] : $settings;
-    }
-}
-
 if(! function_exists('appRoles')) {
     function appRoles()
     {
